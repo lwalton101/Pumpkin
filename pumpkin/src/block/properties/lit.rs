c2007ef1658170c7488a460587f4771eb137b8f9
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2025-03-16 14:35:48 +0000

    remove old lit component

--- pumpkin/src/block/properties/lit.rs
+++
@@ -1,9 +0,0 @@
-use crate::block::properties::BlockProperty;
-use async_trait::async_trait;
-use pumpkin_macros::block_property;
-
-#[block_property("lit")]
-pub struct Lit(bool);
-
-#[async_trait]
-impl BlockProperty for Lit {}
